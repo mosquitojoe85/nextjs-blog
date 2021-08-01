bf2ab39bd002d0ccbf591bd32a276839e027feb2
Brook <mosquitojoe85@gmail.com>
2021-08-01 14:34:10 +0800

    feat: 增加 redux-devtools

--- redux/index.js
+++ redux/index.js
@@ -1,11 +1,14 @@
 
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { createWrapper } from 'next-redux-wrapper';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 
+const composeEnhancers = typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const initStore = (initialState = {}) => {
-  return createStore(reducer, initialState, applyMiddleware(thunk));
+  return createStore(reducer, initialState, composeEnhancers(
+    applyMiddleware(thunk)
+  ));
 };
 
 export const wrapper = createWrapper(initStore, { debug: true });
